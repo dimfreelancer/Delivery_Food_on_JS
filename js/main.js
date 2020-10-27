64e5da84405eb0c0a0d1c213d958b4570756cbcd
Dmitry Freelancer <dimfreelancerdn@gmail.com>
2020-10-28 00:24:39 +0300

    Доставка на js День 1 добавил addEventListener открыть и закрыть модальное окно

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+'use strict';
+console.log('hello day 1');
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
@@ -8,3 +11,31 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+
+
+// day 1
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+
+
+const toggleModalAuth = () => {
+  console.log('modalAuth: ', modalAuth);
+  modalAuth.classList.toggle('is-open');
+}
+
+buttonAuth.addEventListener('click', () => {
+  // modalAuth
+  toggleModalAuth();
+});
+
+closeAuth.addEventListener('click', toggleModalAuth);
+
+
+
+
+
+
+
+
